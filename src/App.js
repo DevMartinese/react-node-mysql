863f1858aab2036671d577e5ffb30d580827b093
devmartinese <devmartinese@gmail.com>
2020-01-18 10:34:27 -0300

    .

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+/*Import Components*/
+import Header from './components/Header'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Header/>
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
