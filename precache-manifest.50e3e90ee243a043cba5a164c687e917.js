1e526761b0befb7d40e1f80f637f27d8132fd3eb
Juanse <Juansemastrangeloo10@gmail.com>
2020-03-14 21:23:03 -0300

    Updates

---
+++ precache-manifest.50e3e90ee243a043cba5a164c687e917.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72f47a81fd31f4a28c2436f7780be686",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a98cf2f15fbcbc90efa",
+    "url": "/static/css/main.93e7f4ea.chunk.css"
+  },
+  {
+    "revision": "b8e585524b712950b6f7",
+    "url": "/static/js/2.a01709e0.chunk.js"
+  },
+  {
+    "revision": "5a98cf2f15fbcbc90efa",
+    "url": "/static/js/main.d9cef3e3.chunk.js"
+  },
+  {
+    "revision": "fa804630da9c2cbea5cc",
+    "url": "/static/js/runtime-main.4807ee4e.js"
+  }
+]);
